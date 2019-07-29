209bec0cb15839704fbf66eaa6e97a7f64b391cc
Punto Digital <Punto Digital@DESKTOP-4C6L2FE>
2019-07-29 16:01:08 -0300

    Agregue dos hola,

---
+++ src/clase23/Clase23.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase23;
+
+/**
+ *
+ * @author Punto Digital
+ */
+public class Clase23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("hola2");
+    }
+    
+}
